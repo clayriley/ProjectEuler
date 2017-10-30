0cd7ea0b75743bc361b8a75c6cbdff0ef1c7624e
clayriley <riley@brandeis.edu>
2017-10-30 14:06:36 -0400

    added explanatory note at top of file

--- product_series.py
+++ product_series.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 '''
+Two implementations of Project Euler problem 8, Largest Product in a Series.
+
 "The four adjacent digits in the 1000-digit number that have the greatest 
 product are 9 × 9 × 8 × 9 = 5832.
 
